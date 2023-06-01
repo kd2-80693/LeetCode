06ee1ed28cf6ebcfe5ce726b4659fdd574215a41
KritagyaChopra <85625671+KritagyaChopra@users.noreply.github.com>
2023-06-01 10:32:43 +0530

    Create 1. Two Sum1.java

---
+++ 1. Two Sum1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0 ; i < nums.length-1 ; i++){
+            int a = nums[i];
+            for(int j = i+1 ; j < nums.length ; j++ ){
+                int b = nums[j];
+                if(a+b==target){
+                    return new int[]{i , j };
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
